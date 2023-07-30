279cc6a48ff68e48ea7771d8e8316a8dcd2c14c6
Apochi Solomon <111003992+Polishedstar@users.noreply.github.com>
2023-07-29 19:43:14 -0700

    lockboxes

---
+++ 0x01-lockboxes /0-lockboxes.py
@@ -0,0 +1,24 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
+
+# Test cases
+boxes1 = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes1))  # Output: True
+
+boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes2))  # Output: True
+
+boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes3))  # Output: False
